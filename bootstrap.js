fc7bce8906a452cf1be584866e92e55e3da35e7f
Jelle De Loecker <jelle@codedor.be>
2014-09-28 20:55:08 +0200

    Remove v0.1 code

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,3 @@
-alchemy.requirePlugin(['menu', 'styleboost', 'acl', 'jquery', 'jsoneditor', 'select2']);
-
 // Define the default options
 var options = {
 
@@ -27,7 +25,7 @@ var options = {
 };
 
 // Inject the user-overridden options
-alchemy.plugins.chimera = alchemy.inject(options, alchemy.plugins.chimera);
+alchemy.plugins.chimera = Object.assign(options, alchemy.plugins.chimera);
 
 // Set the acl placeholder variable
 alchemy.plugins.acl.placeholders.chimeraRouteName = alchemy.plugins.chimera.routename;
@@ -43,120 +41,6 @@ var viewSettings = {
 };
 
 // Send the acl layout options to the client
-alchemy.on('render.callback', function(render, callback) {
-
-	// Only send this data on the initial pageload
-	if (!render.ajax) {
-		render.store('chimera-view-setting', viewSettings);
-	}
-	
-	callback();
-});
-
-// Make sure the chimera-sidebar menu exists
-alchemy.ready(function checkChimeraSidebar() {
-	var Menu = Model.get('Menu');
-
-	Menu.find('first', {conditions: {name: 'chimera-sidebar'}}, function (err, result) {
-		
-		// If no result was found, create one!
-		if (!result.length) {
-			var data = {
-				Menu: {
-					name: 'chimera-sidebar'
-				},
-				MenuPiece: [
-					{
-						"settings" : {
-							"module" : "model_editor",
-							"target" : "",
-							"parent" : "",
-							"order" : 5
-						},
-						"type" : "chimera_module"
-					},
-					{
-						"settings" : {
-							"module" : "json",
-							"target" : "",
-							"parent" : "",
-							"order" : 10
-						},
-						"type" : "chimera_module"
-					}
-				]
-			};
-
-			Menu.save(data, function(err, result) {
-				if (err) {
-					log.error('Failed to create chimera-sidebar menu:');
-					log.error(err);
-				}
-			});
-		}
-
-	});
-
-});
-
-// Make sure the Administrator ACL group exists
-alchemy.ready(function checkChimeraACLGroups() {
-	var AclGroup = Model.get('AclGroup'),
-	    AclPermission = Model.get('AclPermission'),
-	    AllowId = alchemy.ObjectId('52efff0000A1C00002000001'),
-	    DenyId = alchemy.ObjectId('52efff0000A1C00002000000');
-
-	// See if the Superusers allow permission exists
-	AclPermission.find('first', {conditions: {_id: AllowId}}, function(err, result) {
-
-		if (!result.length) {
-			var data = {
-				AclPermission: {
-					"target" : "group",
-					"type" : "url",
-					"parent_name" : "/%chimeraRouteName%(/.*?|)",
-					"child_name" : "",
-					"halt" : false,
-					"order" : 20,
-					"allow" : true,
-					"target_group": alchemy.plugins.acl.SuperUserGroupId,
-					"_id": AllowId
-				}
-			};
-		
-			AclPermission.save(data, function(err, result) {
-				if (err) {
-					log.error('Failed to create Superusers Chimera rule');
-					log.error(err);
-				}
-			});
-		}
-	});
-
-	// See if the deny permission exists
-	AclPermission.find('first', {conditions: {_id: DenyId}}, function(err, result) {
-
-		if (!result.length) {
-			var data = {
-				AclPermission: {
-					"target" : "everyone",
-					"type" : "url",
-					"parent_name" : "/%chimeraRouteName%(/.*?|)", // Anything starting with the chimera path is forbidden
-					"child_name" : "",
-					"halt" : false,
-					"order" : 10,
-					"allow" : false,
-					"_id": DenyId
-				}
-			};
-
-			AclPermission.save(data, function(err, result) {
-				if (err) {
-					log.error('Failed to save permission to restrict users from Chimera pages');
-					log.error(err);
-				}
-			});
-		}
-
-	});
+alchemy.hawkejs.on({type: 'viewrender', status: 'begin', client: false}, function onBegin(viewRender) {
+	viewRender.expose('chimera-view-setting', viewSettings);
 });
