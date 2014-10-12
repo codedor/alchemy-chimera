77f32e5054150390e7ad5167d42613b75844bcbd
Jelle De Loecker <jelle@codedor.be>
2014-10-13 00:47:14 +0200

    Add geopoint field & leaflet code

--- assets/scripts/chimera/chimera.js
+++ assets/scripts/chimera/chimera.js
@@ -1,6 +1,5 @@
-console.log('hello')
-
 hawkejs.scene.on({type: 'set', name: 'pageCentral'}, applySave);
+hawkejs.scene.on({type: 'set', name: 'pageCentral'}, applyGeopoint);
 
 function applySave() {
 
@@ -27,4 +26,49 @@ function applySave() {
 	});
 }
 
-applySave();
+function applyGeopoint() {
+
+	$('.geopoint-view').each(function eachGeopoint() {
+
+		var $this = $(this),
+		    lat,
+		    lng,
+		    map;
+
+		lat = parseFloat($this.data('lat'));
+		lng = parseFloat($this.data('lng'));
+
+		// Skip this map if the coordinates are not numbers
+		if (!isFinite(lat) || !isFinite(lng)) {
+			return;
+		}
+
+		// Add the point to the map
+		map = L.map($this.attr('id'), {
+			dragging: false,
+			touchZoom: false,
+			center: [lat+0.0012, lng],
+			zoomControl: false,
+			attributionControl: false,
+			scrollWheelZoom: 'center',
+			minZoom: 13,
+			maxZoom: 15,
+			zoom: 14
+		});
+
+		L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+			attribution: '',
+			maxZoom: 16
+		}).addTo(map);
+
+		L.marker([lat, lng]).addTo(map);
+	});
+}
+
+$(document).ready(function() {
+	applySave();
+});
+
+$(window).load(function() {
+	applyGeopoint();
+})
